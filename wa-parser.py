0fa83993b1bc5c987c13876c405c92fa6ecfe7c4
sp78dev <sp78dev@gmail.com>
2019-10-21 01:30:01 +0530

    numpy parsing technique added in script but really slow

--- wa-parser.py
+++ wa-parser.py
@@ -7,7 +7,9 @@ import inspect
 import json
 import argparse
 import datetime
+import time
 import pandas as pd
+import numpy as np
 
 
 def chatParser(text_file):
@@ -91,6 +93,122 @@ def chatParser(text_file):
 	return df
 
 
+def chatParserWithNumpy(text_file):
+	"""
+		Parse the chat in the given text file using numpy and give the pandas dataframe
+
+	"""
+
+	prev_matched = ""
+	same_msg = []; full_msg = []; sender = []; message = []; created_date = [];
+
+	try:
+
+		array_data = np.loadtxt(text_file, skiprows=0, dtype='str', delimiter='\n', encoding='utf8')
+		firstline = array_data[0]
+
+		with_ampm_regex = r'(\d{1,2}\/\d{2}\/\d{2}), (\d{1,2}:\d{2}) (AM|PM|am|pm)'
+		without_ampm_regex = r'(\d{1,2}\/\d{2}\/\d{2}), (\d{1,2}:\d{2})'
+
+		with_ampm = False
+
+		if re.match(with_ampm_regex, firstline):
+			with_ampm = True
+			chat_regex = re.compile(r'((\d{1,2}\/\d{2}\/\d{2}), (\d{1,2}:\d{2}) (AM|PM|am|pm) - ([\w\s]+): ((.|\n)*))', re.MULTILINE)
+			time_regex = r'(\d{1,2}\/\d{2}\/\d{2}), (\d{1,2}:\d{2})'
+		
+		elif re.match(without_ampm_regex, firstline):
+			chat_regex = re.compile(r'((\d{1,2}\/\d{2}\/\d{2}), (\d{1,2}:\d{2}) - ([\w\s]+): ((.|\n)*))', re.MULTILINE)
+			time_regex = r'(\d{1,2}\/\d{2}\/\d{2}), (\d{1,2}:\d{2})'
+
+		else:
+			print(json.dumps({"message": "Datetime format not matched", 'ok':0}, indent=4))
+			sys.exit(1)
+
+		# convert the multline message into single message
+		# for ind, i in enumerate(array_data):
+
+		# 	if re.match(time_regex, i):
+		# 		if same_msg:
+		# 			same_msg = np.insert(same_msg, 0, prev_matched)
+		# 			full_msg = np.append(full_msg, '\n'.join(same_msg))
+		# 			same_msg = []
+		# 		prev_matched = i
+		# 		full_msg=np.append(full_msg, i)
+		# 	else:
+		# 		same_msg.append(i)
+		# 		if ind == len(array_data)- 1:
+		# 			if same_msg:
+		# 				same_msg.insert(0, prev_matched)
+		# 				full_msg = np.append(full_msg, '\n'.join(same_msg))
+		# 			continue
+		# 		if len(same_msg) == 1 and len(same_msg) > 0:
+		# 			full_msg=np.delete(full_msg,-1)
+
+		# if with_ampm:
+
+		# 	for j in full_msg:
+		# 		for m in chat_regex.finditer(j):
+		# 			parsed_date = datetime.datetime.strptime(m.group(2)+" "+m.group(3)+" "+m.group(4), "%d/%m/%y %I:%M %p")
+		# 			created_date = np.append(created_date, parsed_date)
+		# 			sender = np.append(sender, m.group(5).encode('utf-8').strip())
+		# 			message = np.append(message, m.group(6).encode('utf-8').strip())
+		# else:
+
+		# 	for j in full_msg:
+		# 		for m in chat_regex.finditer(j):
+		# 			parsed_date = datetime.datetime.strptime(m.group(2)+" "+m.group(3), "%m/%d/%y %H:%M")
+		# 			created_date = np.append(created_date, parsed_date)
+		# 			sender = np.append(sender, m.group(4).encode('utf-8').strip())
+		# 			message = np.append(message, m.group(5).encode('utf-8').strip())
+
+		for ind, i in enumerate(array_data):
+
+				if re.match(time_regex, i):
+					if same_msg:
+						same_msg.insert(0, prev_matched)
+						full_msg.append('\n'.join(same_msg))
+						same_msg = []
+					prev_matched = i
+					full_msg.append(i)
+				else:
+					same_msg.append(i)
+					if ind == len(array_data)-1:
+						if same_msg:
+							same_msg.insert(0, prev_matched)
+							full_msg.append('\n'.join(same_msg))
+						continue
+					if len(same_msg) == 1 and full_msg:
+						del full_msg[-1]
+
+		if with_ampm:
+
+			for j in full_msg:
+				for m in chat_regex.finditer(j):
+					parsed_date = datetime.datetime.strptime(m.group(2)+" "+m.group(3)+" "+m.group(4), "%d/%m/%y %I:%M %p")
+					created_date.append(parsed_date)
+					sender.append(m.group(5).encode('utf-8').strip())
+					message.append(m.group(6).encode('utf-8').strip())
+		else:
+
+			for j in full_msg:
+				for m in chat_regex.finditer(j):
+					parsed_date = datetime.datetime.strptime(m.group(2)+" "+m.group(3), "%m/%d/%y %H:%M")
+					created_date.append(parsed_date)
+					sender.append(m.group(4).encode('utf-8').strip())
+					message.append(m.group(5).encode('utf-8').strip())
+		df = pd.DataFrame(zip(sender, message, created_date), columns=['sender', 'message', 'created_at'])
+		df['message'], df['sender'] = df['message'].str.decode("utf-8"), df['sender'].str.decode("utf-8")
+
+	except Exception as e:
+		print(json.dumps({"message": "Error at line {0} -- {1}".format(sys.exc_info()[-1].tb_lineno, str(e)), 'ok':0}, indent=4))
+		sys.exit(1)
+
+	return df
+
+	# return pd.DataFrame()
+
+
 def parse_args():
 	"""
 	Get the arguments passed while running the program.
@@ -99,10 +217,13 @@ def parse_args():
 
 	parser = argparse.ArgumentParser(add_help=True)
 	parser.add_argument('-p', '--path', type=str, help='Give the exported chat file path',
-						action="store")
+						action="store", required= False)
 
-	parser.add_argument('-o', "--output", choices=['json', 'csv'], action="store",
+	parser.add_argument('-s', "--save", choices=['json', 'csv'], action="store",
 						help='Get the result in json or csv file')
+
+	parser.add_argument("-o", "--order", choices=['asc', 'desc'], help="To order the chat by time",
+						action="store")
 
 	return parser.parse_args()
 
@@ -117,7 +238,7 @@ if __name__ == '__main__':
 			sys.exit(1)
 
 		# DB Initialization process
-		if args.path and args.output:
+		if args.path and args.save:
 			
 			if os.path.isfile(args.path):
 				filename, file_extension = os.path.splitext(args.path)
@@ -126,14 +247,23 @@ if __name__ == '__main__':
 					print(json.dumps({"message": "Unsupported file extension", 'ok':0}, indent=4))
 					sys.exit(1)
 
-				df = chatParser(args.path)
+				start = time.time()
+				print("start time", start)
+				df = chatParser(args.path) #chatParserWithNumpy(args.path)
+				end = time.time()
+				print("start time", end)
+				print("parse time", end - start)
 
-				if args.output == 'json':
-					output_path = filename+ '.json'
-					df.to_json(output_path, orient='records', date_format = "%Y-%m-%d %H:%M:%S")
+				if args.order == 'desc':
+					df = df.iloc[::-1]
+
+				if args.save == 'json':
+					output_path = filename + '.json'
+					df['created_at'] = df['created_at'].dt.strftime('%Y-%m-%d %H:%M:%S')
+					df.to_json(output_path, orient='records')
 					print(json.dumps({"message": "JSON File successfully created", 'ok':1}, indent=4))
 
-				elif args.output == 'csv':
+				elif args.save == 'csv':
 					output_path = filename+ '.csv'
 					df.to_csv(output_path, index= False, date_format= "%Y-%m-%d %H:%M:%S")
 					print(json.dumps({"message": "CSV File successfully created", 'ok':1}, indent=4))
@@ -142,6 +272,7 @@ if __name__ == '__main__':
 				print(json.dumps({"message": "Given file path not exist", 'ok':0}, indent=4))
 
 		else:
+
 			print(json.dumps({"message": "Give the proper arguments", 'ok':0}, indent=4))
 
 	except Exception as e:
